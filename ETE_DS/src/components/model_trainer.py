0b114acea9967b7dc81568d5a65a6591219eb861
paradox-shivansh <theaverageguy19@gmail.com>
2026-02-17 16:15:20 +0530

    Hyper-parameter tuning. tuned the models in the trainer and added a function for the parameters in the util file

--- ETE_DS/src/components/model_trainer.py
+++ ETE_DS/src/components/model_trainer.py
@@ -39,7 +39,35 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
             }
             
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2']
+                },
+                "Random Forest": {
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2']
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5,7,9,11],
+                    'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [100, 200]
+                }
+            }
+            
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
             
             # to get the best model score from the dictionary
             best_model_score = max(sorted(model_report.values()))
--- ETE_DS/src/utils.py
+++ ETE_DS/src/utils.py
@@ -7,6 +7,9 @@ from sklearn.metrics import r2_score
 import pickle
 import dill
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
+
+
 
 
 def save_object(file_path, obj):
@@ -20,12 +23,19 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, para, cv=3, n_jobs=-1, verbose=2)
+            gs.fit(X_train, y_train)
+            # model.fit(X_train, y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             
             y_train_pred = model.predict(X_train)

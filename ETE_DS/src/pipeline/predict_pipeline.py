356014e937e5bdf9f61ce65c6d14b5a50f5d1599
paradox-shivansh <theaverageguy19@gmail.com>
2026-02-19 23:03:13 +0530

    done before work of pipeline

---
+++ ETE_DS/src/pipeline/predict_pipeline.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from sklearn.pipeline import Pipeline
+import numpy as np
+from src.logger import logger
+from src.exception import CustomException
+from src.utils import load_object
+
